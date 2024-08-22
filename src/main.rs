567673499586b046fe21810b1da2234b3e9de17b
Bornwell Matembudze <bornwell@leapinventions.co.zw>
2024-08-22 23:23:26 +0200

    Using the println! macro

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main(){
-let stdout = stdout();
-let message = String::from("Hello World! I am Bornwell");
-let width = message.chars().count();
-let mut writer = BufWriter::new(stdout.lock());
-say(&message, width, &mut writer).unwrap();
+	let stdout = stdout();
+	let message = String::from("Hello World! I am Bornwell");
+	let width = message.chars().count();
+	let mut writer = BufWriter::new(stdout.lock());
+	println!("Seems the macro output below is printed before the say function despite being the last line");
+	say(&message, width, &mut writer).unwrap();
+	println!("I am a Rustacean now. Given this new identity by the println! macro. Watch the extra ! otherwise ferris will bit yah");
 }
